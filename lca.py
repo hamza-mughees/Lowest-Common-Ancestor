a6c286c49cba3d010cf66f31ec1fdbeb394251be
Hamza Mughees <hamzamughees@gmail.com>
2020-10-17 00:09:31 +0100

    Update lca.py

--- lca.py
+++ lca.py
@@ -25,34 +25,3 @@ class BinaryTree:
         if right_subtree is None: return left_subtree
         
         return curr_node
-
-def lca(p:'Node', q:'Node'):
-    print('LCA of node {} and {} is {}'.format(p.value, q.value, t.lca(p, q).value))
-
-t = BinaryTree(3)
-r = t.root
-
-r.left = Node(5)
-r.right = Node(1)
-r.left.left = Node(6)
-r.left.right = Node(2)
-r.right.left = Node(0)
-r.right.right = Node(8)
-r.left.right.left = Node(7)
-r.left.right.right = Node(4)
-
-'''
-The above code generates the following tree:
-            3
-          /   \
-        5       1
-       / \     / \
-      6   2   0   8
-         / \
-        7   4
-'''
-
-lca(r.left, r.right)                            # 5, 1 => 3
-lca(r.left.left, r.left.right.right)            # 6, 4 => 5
-lca(r.right, r.right.left)                      # 1, 0 => 1
-lca(r.left.right.left, r.left.right.right)      # 7, 4 => 2
